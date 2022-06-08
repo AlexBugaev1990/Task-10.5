699be3fb898b2a67177885df1ae72b553fa7f3d1
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-06-08 20:14:08 +0300

    Добавьте файлы проекта.

---
+++ Task 10.5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Программка для подсчета суммы двух чисел");
+        while (true)
+        {
+            try
+            {
+                ICalculator calculator = new Calc();
+                                
+                Console.WriteLine("Введите 1-ое число");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите 2-ое число");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                calculator.Summ(a, b);
+
+            }
+            catch (OverflowException e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Вы ввели слишком большое число");
+            }
+            catch (FormatException e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine($"Не верный формат данных");
+            }
+
+        }
+    }
+
+    public interface ICalculator
+    {
+        void Summ(int a, int b);
+
+    }
+
+
+    public class Calc : ICalculator
+    {
+        void ICalculator.Summ(int a, int b)
+        {
+            Console.WriteLine("Ответ = {0}", a + b);
+        }
+    }
+
+}  
